1538ea2fbc56e0c2c38012bf98285a27ee1fff33
takeda tamako <ttetama@icloud.com>
2022-12-11 12:12:28 +0900

    Initial Commit

---
+++ fashionSNS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fashionSNS
+//
+//  Created by 竹田珠子 on 2022/12/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
